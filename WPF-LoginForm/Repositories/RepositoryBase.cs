7dd7b26577f334283fa7c409986da446040715d4
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-08-15 00:07:43 +0600

    Add project files.

---
+++ WPF-LoginForm/Repositories/RepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WPF_LoginForm.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string _connectionString;
+        public RepositoryBase()
+        {
+            _connectionString = "Data Source=AMINUL\\SQLEXPRESS;Initial Catalog=MVVMLoginDb;Integrated Security=True";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
---
+++ WPF-LoginForm/Views/MainView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace WPF_LoginForm.Views
+{
+
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+       
+        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+       
+    }
+}
